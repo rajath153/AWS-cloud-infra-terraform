2f41c2254f6a6e5b23f494b7fd2c813694adf7ae
rajath153 <44300754+rajath153@users.noreply.github.com>
2021-02-15 11:58:06 -0500

    contains declaration of variables used by the resources.

--- variables.tf
+++ variables.tf
@@ -1,34 +1,38 @@
 
-variable "region"{
+variable "your_AWS_region"{
     type = string
-    
+
 }
-variable "ac_key" {
+variable "your_AWS_account_access_key" {
   type=string
 }
 
-variable "sec_key" {
+variable "your_AWS_account_secret_key" {
     type=string
 }
 
 variable "image" {
-    type=string
-    default = "ami-01aab85a5e4a5a0fe" #Amazon Linux 2 AMI (HVM)
+    description = "image of the EC2 instance"
+    type        = string
+    default     = "ami-01aab85a5e4a5a0fe" #Amazon Linux 2 AMI (HVM)
 }
 
 variable "inst_type" {
-    type = string
-    default = "t2.micro"
+    description = "EC2 instance type"
+    type        = string
+    default     = "t2.micro"
 }
 
 variable "inst_tag" {
-  type=map(string)
-  default={
+  description = "EC2 instance tag"
+  type        =map(string)
+  default     = {
     Name="instance-1"
     }
 }
 
 variable "vpc_tag" {
+  description = "VPC network tag"
   type = map(string)
   default = {
     Name = "vpc-a"
